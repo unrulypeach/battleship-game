a69d8658b23efd9daf1f7b994dc64903a0930d3f
unrulypeach <gingeraleisthebest@gmail.com>
2022-07-31 18:46:55 -0400

    added drag evenet listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-expressions */
 import ShipFactory from './shipFactory';
 import GameboardFactory from './gameboard';
 import player from './player';
@@ -56,18 +58,49 @@ const game = () => {
   };
 };
 
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData('text', ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  const data = ev.dataTransfer.getData('text');
+  const ship = document.getElementById(data);
+  ship.children.forEach((shipPc) => {
+    shipPc.target.append(shipPc);
+  });
+  for (const [key] of Object.entries(ship.children)) {
+
+  }
+}
+
 (() => {
   const boardContainerList = document.querySelectorAll('.boardCont');
   boardContainerList.forEach((x) => {
     const div = document.createElement('div');
     div.classList.add('boardCells');
+    div.id = 'bc';
     for (let i = 0; i < 100; i += 1) {
       const gridCell = document.createElement('span');
-      gridCell.id = i;
+      gridCell.id = `cell${i}`;
       gridCell.classList.add('cell');
       gridCell.style.height = '20px';
+      gridCell.addEventListener('dragover', allowDrop);
+      gridCell.addEventListener('drop', drop);
       div.append(gridCell);
     }
     x.appendChild(div);
+  });
+})();
+
+(() => {
+  const pieces = document.querySelectorAll('.ship');
+  pieces.forEach((x) => {
+    // x.draggable = 'true';
+    x.addEventListener('dragstart', drag);
   });
 })();
